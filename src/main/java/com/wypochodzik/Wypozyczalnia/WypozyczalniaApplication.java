1eba58766e1d774cc77a42c3ffd732fc8736aa88
PawelHanzlik <hanzlik_pawel@o2.pl>
2020-11-29 18:46:57 +0100

    init

---
+++ src/main/java/com/wypochodzik/Wypozyczalnia/WypozyczalniaApplication.java
@@ -0,0 +1,13 @@
+package com.wypochodzik.Wypozyczalnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WypozyczalniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WypozyczalniaApplication.class, args);
+	}
+
+}
